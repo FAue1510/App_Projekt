8d073084339052408300c89af0592513634ea2eb
flori <florianaue2001@gmail.com>
2021-11-18 14:51:11 +0100

    Switch to ProfileActivity

--- app/src/main/java/activity/HomeActivity.java
+++ app/src/main/java/activity/HomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -65,5 +66,9 @@ public class HomeActivity extends Activity {
 
         Intent intent = new Intent(HomeActivity.this, DataViewActivity.class);
         startActivity(intent);
+    }
+
+    public void switchToProfile(View view) {
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
     }
 }
---
+++ app/src/main/java/activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a21q4_app_projekt.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
