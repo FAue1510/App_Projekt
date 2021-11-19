1076e99e7172e02daa05f752e3aca696c0ab57ab
nicorott <93339754+nicorott@users.noreply.github.com>
2021-11-19 16:06:56 +0100

    settings

---
+++ app/src/main/java/activity/PersonalDataActivity.java
@@ -0,0 +1,20 @@
+package activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.a21q4_app_projekt.R;
+import model.Account;
+
+public class PersonalDataActivity extends AppCompatActivity {
+
+    private Account acc;
+    public String name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_data);
+        name = acc.getFirstName();
+    }
+}
